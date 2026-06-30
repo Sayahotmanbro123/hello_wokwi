1bafe27389a7f067e87cd751cd5fb9e3a3019a38
Hotman Pangaribuan <hotmanpangiribuan17@gmail.com>
2026-06-30 11:33:26 +0700

    Initial commit - setup wokwi simulation

---
+++ main/hello_world_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main() {
+  printf("Hello, Wokwi!\n");
+  while (true) {
+    printf("Hello, Wokwi!\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
